aa5a97765ab49e63dae0b344ebc6f3020b5314c0
KazuSoda <104375725+KazuSoda@users.noreply.github.com>
2022-04-25 17:15:04 +0200

    Initial deployment

---
+++ app.js
@@ -0,0 +1,216 @@
+function AfficherMasquer1()
+{
+divInfo = document.getElementById('saison1');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer2()
+{
+divInfo = document.getElementById('saison2');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer3()
+{
+divInfo = document.getElementById('saison3');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer4()
+{
+divInfo = document.getElementById('saison4');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer5()
+{
+divInfo = document.getElementById('saison5');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer6()
+{
+divInfo = document.getElementById('saison6');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer7()
+{
+divInfo = document.getElementById('saison7');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer8()
+{
+divInfo = document.getElementById('saison8');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer9()
+{
+divInfo = document.getElementById('saison9');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer10()
+{
+divInfo = document.getElementById('saison10');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer11()
+{
+divInfo = document.getElementById('saison11');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer12()
+{
+divInfo = document.getElementById('saison12');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer13()
+{
+divInfo = document.getElementById('saison13');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer14()
+{
+divInfo = document.getElementById('saison14');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer15()
+{
+divInfo = document.getElementById('saison15');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer16()
+{
+divInfo = document.getElementById('saison16');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer17()
+{
+divInfo = document.getElementById('saison17');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer18()
+{
+divInfo = document.getElementById('saison18');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer19()
+{
+divInfo = document.getElementById('saison19');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer20()
+{
+divInfo = document.getElementById('saison20');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer21()
+{
+divInfo = document.getElementById('saison21');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer22()
+{
+divInfo = document.getElementById('saison22');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer23()
+{
+divInfo = document.getElementById('saison23');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
+function AfficherMasquer24()
+{
+divInfo = document.getElementById('saison24');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'flex';
+else
+divInfo.style.display = 'none';
+}
